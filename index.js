8478ff838d09359e8f247eab0dc8ba49987abd49
Luhenrique09 <luhenrique1100@gmail.com>
2022-11-04 17:51:47 -0300

    final version

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 
-
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -11,8 +10,9 @@ const tweets = []
 const user = []
 
 app.get('/tweets', (req, res) => { 
- 
-    res.send(tweets) 
+
+    res.send(tweets.slice(0,10)) 
+    
 });
 
 app.post('/sign-up', (req, res) => {
@@ -27,13 +27,13 @@ app.post('/sign-up', (req, res) => {
 })
 
 app.post('/tweets', (req, res) => {
-    const url = user.find((obj) => obj.username === req.body.username)
+    const url = user.find((obj) => obj.username === req.body.username)  
     const newTweet = {
         username: req.body.username,
         avatar: url.avatar,
         tweet: req.body.tweet
     }
-    tweets.push(newTweet)
+    tweets.unshift(newTweet)
     res.send('OK') 
 });
 
